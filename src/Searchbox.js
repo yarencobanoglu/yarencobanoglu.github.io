d771e1e354a5a577494c4304c4831fd4ca6b63bf
yarencobanoglu <yarencobanoglu7@gmail.com>
2022-06-05 18:44:41 +0300

    dosylar yükledi

---
+++ src/Searchbox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Searchbox = () => {
+    return ( 
+        <div>
+            <input 
+            className='pa3 ba b--green bg-lightest-blue' 
+            type="search" 
+            name="" 
+            id="" 
+            placeholder='Search Robot'
+            />
+        </div>
+     );
+}
+ 
+export default Searchbox;
